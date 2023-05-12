081e832886c29cc2ece81165927d0ad6f51d54fe
bbpbrian <bbihn04@gmail.com>
2023-05-12 11:30:54 +0200

    metode2

---
+++ M5 EXAMEN/Suma2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package suma2;
+
+/**
+ *
+ * @author alumne
+ */
+public class Suma2 {
+
+
+ public int sumaValorAbsolut(int num2) {
+            int absolutNum1 = Math.abs(num1);
+            int absolutNum2 = Math.abs(num2);
+            return absolutNum1 + absolutNum2;
+        }
+
+        
+
+}
