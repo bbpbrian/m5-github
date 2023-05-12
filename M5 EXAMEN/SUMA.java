c4fc681adab0b154a68c304f5579ba037570ea95
bbpbrian <bbihn04@gmail.com>
2023-05-12 11:30:33 +0200

    metodo1

---
+++ M5 EXAMEN/SUMA.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package suma;
+
+/**
+ *
+ * @author alumne
+ */
+public class SUMA {
+
+        private int num1;
+
+        public SUMA(int num1) {
+            this.num1 = num1;
+        }
+
+        public int getNum1() {
+            return num1;
+        }
+   
+}
