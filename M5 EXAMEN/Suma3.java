8df226770db5e521ca83484a466bbbfd601a443b
bbpbrian <bbihn04@gmail.com>
2023-05-12 11:31:37 +0200

    metode3

---
+++ M5 EXAMEN/Suma3.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package suma3;
+
+/**
+ *
+ * @author alumne
+ */
+public class Suma3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
+public int sumar(int num2) {
+            return num1 + num2;
+        }
+
+  
